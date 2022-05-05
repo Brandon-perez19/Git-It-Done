f95cecd9b469b574bb572f1e83d57038294d9620
Brandon <brandonperez@brandons-mbp.lan>
2022-05-05 16:41:54 -0400

    cleaned up/restructed code.

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,15 +1,10 @@
 var getUserRepos = function(user){
-    // //calls to ServerSide API to retreive/fetch data
-    // var response = fetch("https://api.github.com/users/octocat/repos");
-    
-    // //see the JSON file retrived by the fetch call in the console. Returns Promise Object
-    // console.log(response);
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-    //Since a project object was returned, we use the .then method
-    fetch("https://api.github.com/users/octocat/repos").then(function(response){
-        //formats the response data into JSON. If a resource returns non-JSON data text() would be used instead
-        //json()method returns another promise. Use then()
-        response.json().then(function(data){
+    //make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
             console.log(data);
         });
     });
