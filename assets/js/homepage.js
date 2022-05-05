8e0e0c518b71833422254ecc74c31dddf325e271
Brandon <brandonperez@brandons-mbp.lan>
2022-05-05 16:38:54 -0400

    set-up fetch to return JSON object.

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,11 +1,19 @@
-var getUserRepos = function(){
-    // //Test to see if function worked
-    // console.log("function was called");
+var getUserRepos = function(user){
+    // //calls to ServerSide API to retreive/fetch data
+    // var response = fetch("https://api.github.com/users/octocat/repos");
+    
+    // //see the JSON file retrived by the fetch call in the console. Returns Promise Object
+    // console.log(response);
 
-    //calls to ServerSide API to retreive/fetch data
-    fetch("https://api.github.com/users/octocat/repos");
+    //Since a project object was returned, we use the .then method
+    fetch("https://api.github.com/users/octocat/repos").then(function(response){
+        //formats the response data into JSON. If a resource returns non-JSON data text() would be used instead
+        //json()method returns another promise. Use then()
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
 };
 
-getUserRepos();
+getUserRepos(user);
 
-//ping api.github.com is a terminal command to verify api endpoint/ test connection itself
