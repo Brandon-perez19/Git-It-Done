d205c41f2f22b9d7de5fac860913fe5c354e4d1b
Brandon <brandonperez@brandons-mbp.lan>
2022-05-08 23:57:29 -0400

    added language buttons to homepage and created functionality to search for repo based on coding language.

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,22 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+var getFeaturedRepos = function(language){
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                console.log(data);
+                displayRepos(data.items, language);
+            });
+
+        } else {
+            alert('Error: Github User Not Found');
+        }
+    })
+}
 
 var formSubmitHandler = function(event) {
     //prevent pages from refreshing when submitting form
@@ -84,5 +100,16 @@ var displayRepos = function(repos, searchTerm){
     }
 };
 
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    if (language){
+        getFeaturedRepos(language);
+
+        //clear old content
+        repoContainerEl.textContent = "";
+    };
+};
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 userFormEl.addEventListener("submit", formSubmitHandler);
 
